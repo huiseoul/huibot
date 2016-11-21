4738c3e05542aca9ff7a8fac0f4da92b84477f0a
Jitae Kim <originerd@gmail.com>
2016-11-21 12:34:20 +0900

    Add adater as slack to binary

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "huibot" --alias "후이봇" %*
+node_modules\.bin\hubot.cmd -a slack --name "huibot" --alias "후이봇" %*
